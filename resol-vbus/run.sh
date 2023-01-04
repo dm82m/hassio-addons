cdf133c735ddcfc407edfe9bb93fb454b1f42d61
dm82m <dirk@maucher-online.de>
2023-01-04 16:49:16 +0100

    mouting usb serial devices into the container; increased version to 0.0.3

--- resol-vbus/run.sh
+++ resol-vbus/run.sh
@@ -2,5 +2,10 @@
 
 cd /bin/resol-vbus/examples/json-live-data-server
 cp /config/addons/config.js .
-node --version
+echo "--- VERSIONS ---"
+echo -n "nodejs version: " && node --version
+echo -n "npm version: " && npm --version
+echo "--- DEVICES ---"
+ls /dev/tty*
+echo "--- json-live-data-server ---"
 node index.js
