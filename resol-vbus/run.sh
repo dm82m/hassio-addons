6adfa2cf4d0226b486f8e43cd80c160ad9d9eacf
dm82m <dirk@maucher-online.de>
2023-05-18 10:07:07 +0200

    #8 fix for ls command that the shell script runs through even if no ttyUSB device is found

--- resol-vbus/run.sh
+++ resol-vbus/run.sh
@@ -3,10 +3,11 @@
 cd /bin/resol-vbus/examples/json-live-data-server
 cp /config/addons/config.js .
 echo "--- VERSIONS ---"
-echo "add-on version: 0.0.5"
+echo "add-on version: 0.0.6"
 echo -n "nodejs version: " && node --version
 echo -n "npm version: " && npm --version
 echo "--- DEVICES ---"
-ls /dev/ttyUSB*
+ls /dev/ttyUSB* || true
+ls /dev/ttyACM* || true
 echo "--- json-live-data-server ---"
 node index.js
