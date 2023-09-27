8d11124ca78e4446540abbfe25dfa38df691ddeb
dm82m <dirk@maucher-online.de>
2023-09-27 09:15:51 +0200

    Neolink: - reworked the dual mode and using the new mode that Neolink is providing - ATTENTION: there is a small change to the Neolink config file that might thow you off, the mqtt server is now defined globally rather than per camera, so please check the Neolink documentation: https://github.com/QuantumEntangledAndy/neolink and modify your config accordingly

--- neolink/run.sh
+++ neolink/run.sh
@@ -6,7 +6,7 @@ CONFIG_PATH=/data/options.json
 MODE=$(jq --raw-output '.mode // empty' $CONFIG_PATH)
 
 echo "--- VERSIONS ---"
-echo "add-on version: 0.0.6"
+echo "add-on version: 0.0.7"
 echo -n "neolink version: " && neolink --version
 echo "neolink mode: ${MODE}"
 echo "ATTENTION: if you expected a newer Neolink version, please reinstall this Add-on!"
@@ -22,11 +22,7 @@ case $MODE in
     ;;
 
   dual)
-    neolink rtsp --config /config/addons/neolink.toml &
-
-    neolink mqtt --config /config/addons/neolink.toml &
-
-    wait
+    neolink mqtt-rtsp --config /config/addons/neolink.toml
     ;;
 
   *)
