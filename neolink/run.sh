2dd3c1b40f620e250e60fbbd2d4d20f3137fd553
QuantumEntangledAndy <sheepchaan@gmail.com>
2023-10-27 16:48:59 +0700

    Add log level config option

--- neolink/run.sh
+++ neolink/run.sh
@@ -4,6 +4,7 @@ set -e
 CONFIG_PATH=/data/options.json
 
 MODE=$(jq --raw-output '.mode // empty' $CONFIG_PATH)
+LOG=$(jq --raw-output '.log // empty' $CONFIG_PATH)
 
 echo "--- VERSIONS ---"
 echo "add-on version: 0.0.7"
@@ -11,6 +12,24 @@ echo -n "neolink version: " && neolink --version
 echo "neolink mode: ${MODE}"
 echo "ATTENTION: if you expected a newer Neolink version, please reinstall this Add-on!"
 echo "--- Neolink ---"
+
+case $LOG in
+  debug)
+    export RUST_LOG="neolink=debug"
+    ;;
+  info)
+    export RUST_LOG="neolink=debug"
+    ;;
+  warn)
+    export RUST_LOG="neolink=warn"
+    ;;
+  error)
+    export RUST_LOG="neolink=error"
+    ;;
+  *)
+    echo "Unknown log level"
+    ;;
+esac
 
 case $MODE in
   rtsp)
