e548299f48688b4f6875bef9c97fb640e5e4afea
dm82m <dirk@maucher-online.de>
2025-02-10 19:10:16 +0100

    resol-vbus: moving config file to new location '/addon_configs/<slug>_resol-vbus'. the old config file in '/config/addons/' will be renamend from 'config.js' to 'config.js.migrated' and needs to be manually removed

--- resol-vbus/run.sh
+++ resol-vbus/run.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/with-contenv bashio
 
+if [ -f "/homeassistant/addons/config.js" ]; then
+    echo "Migrating '/homeassistant/addons/config.js' to '/addon_configs/<slug>_resol-vbus/config.js'"
+    cp /homeassistant/addons/config.js /config/
+    mv /homeassistant/addons/config.js /homeassistant/addons/config.js.migrated
+fi
+
 cd /bin/resol-vbus/examples/json-live-data-server
-cp /config/addons/config.js .
+cp /config/config.js .
 echo "--- VERSIONS ---"
-echo "add-on version: 0.0.7"
+echo "add-on version: 0.0.8"
 echo -n "nodejs version: " && node --version
 echo -n "npm version: " && npm --version
 echo "--- DEVICES ---"
