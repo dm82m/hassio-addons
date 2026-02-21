66587e3ec24576ea2a015e6dac42b4d6474750b7
dm82m <dirk@maucher-online.de>
2026-02-21 15:51:38 +0100

    thank you linting god! ;-)

--- nzb-monkey-go/app.py
+++ nzb-monkey-go/app.py
@@ -9,14 +9,14 @@ tab1, tab2 = st.tabs(["🚀 Process Links", "⚙️ Configuration"])
 
 # --- TAB 1: LINKS VERARBEITEN ---
 with tab1:
-    col1, col2 = st.columns([1, 10]) # Das Logo bekommt 1/11 des Platzes
+    col1, col2 = st.columns([1, 10])  # Das Logo bekommt 1/11 des Platzes
 
     with col1:
         # Das Bild laden. width=60 sorgt dafür, dass es klein bleibt
         st.image("icon.png", width=60)
 
     with col2:
-      st.title("NZB-Monkey-Go")
+        st.title("NZB-Monkey-Go")
 
     mode = st.radio("Select Mode:", ("Standard", "Direct Search"), horizontal=True)
     script = "/n.sh" if mode == "Standard" else "/nd.sh"
